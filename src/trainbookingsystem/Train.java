503bfaefde7db50dbceb21faebf1a169cabac69c
AnhQuan2208 <120321862+aq2208@users.noreply.github.com>
2025-04-20 16:30:14 +0700

    Initialize

---
+++ src/trainbookingsystem/Train.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trainbookingsystem;
+
+/**
+ *
+ * @author DELL
+ */
+public class Train {
+    private String tcode;
+    
+    private String name;
+    
+    private String dstation;
+}
